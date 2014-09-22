52b21ca26c7bf48c1e54bb1af11f3c77e055bbce
wshino <wshinohara@gmail.com>
2014-09-22 18:11:13 +0900

    add DiscardServer

--- src/main/scala/io/netty/example/discard/DiscardServer.scala
+++ src/main/scala/io/netty/example/discard/DiscardServer.scala
@@ -8,9 +8,6 @@ import io.netty.channel.{ChannelOption, ChannelInitializer, EventLoopGroup}
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
 
-/**
- * Created by JP16163 on 2014/09/22.
- */
 class DiscardServer(p:Int) {
 
   private val port: Int = p
