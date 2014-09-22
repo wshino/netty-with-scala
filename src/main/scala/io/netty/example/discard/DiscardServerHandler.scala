1636d0c51b2b8eebcfaca4b2d2e42d57da4ca327
wshino <wshinohara@gmail.com>
2014-09-22 19:46:14 +0900

    echo server

--- src/main/scala/io/netty/example/discard/DiscardServerHandler.scala
+++ src/main/scala/io/netty/example/discard/DiscardServerHandler.scala
@@ -4,22 +4,24 @@ import io.netty.buffer.ByteBuf
 import io.netty.channel.{ChannelHandlerContext, ChannelHandlerAdapter}
 import io.netty.util.ReferenceCountUtil
 
-/**
- * Created by JP16163 on 2014/09/22.
- */
 class DiscardServerHandler extends ChannelHandlerAdapter{
 
-  override def channelRead(ctx: ChannelHandlerContext, msg: Object) = {
-    val in = msg.asInstanceOf[ByteBuf]
-    try {
-      while (in.isReadable) {
-        System.out.println(in.readByte())
-        System.out.flush()
-      }
-    } finally {
-      ReferenceCountUtil.release(msg)
+//  override def channelRead(ctx: ChannelHandlerContext, msg: Object) = {
+//    val in = msg.asInstanceOf[ByteBuf]
+//    try {
+//      while (in.isReadable) {
+//        System.out.println(in.readByte())
+//        System.out.flush()
+//      }
+//    } finally {
+//      ReferenceCountUtil.release(msg)
+//    }
+//  }
+    // echo server
+    override def channelRead(ctx: ChannelHandlerContext, msg: Object) = {
+      ctx.write(msg)
+      ctx.flush()
     }
-  }
 
 
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) = {
