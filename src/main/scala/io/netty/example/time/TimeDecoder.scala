a7d548e54f5b6b6f1cb513a5ed42c0111da0083f
wshino <wshinohara@gmail.com>
2014-09-23 11:55:22 +0900

    use replaying decoder

--- src/main/scala/io/netty/example/time/TimeDecoder.scala
+++ src/main/scala/io/netty/example/time/TimeDecoder.scala
@@ -4,14 +4,10 @@ import java.util
 
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
-import io.netty.handler.codec.ByteToMessageDecoder
+import io.netty.handler.codec.{ReplayingDecoder, ByteToMessageDecoder}
 
-class TimeDecoder extends ByteToMessageDecoder {
+class TimeDecoder extends ReplayingDecoder[Unit] {
   override def decode(ctx: ChannelHandlerContext, in: ByteBuf, out: util.List[AnyRef]): Unit = {
-    if (in.readableBytes() < 4) {
-      return
-    }
-
-    out.add(in.readBytes(4))
+     out.add(in.readBytes(4))
   }
 }
